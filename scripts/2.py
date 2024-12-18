beb8295d3db6f7ed3d836f4ff3e49e5c18cf8e91
asuka-laptop <>
2024-12-18 18:11:54 +0100

    day 2 part 2 in progress

--- scripts/2.py
+++ scripts/2.py
@@ -25,4 +25,25 @@ def inc_analyze(file):
 
 print(step_analyze(inc_analyze(file)))
 
-######################## part2
+######################## part2 - NOT DONE
+def lenient_report(file):
+    count = 0
+
+    for report in file:
+        # Check how many times the series is inceasing or decreasing
+        inc_inconsistencies = sum(1 for i in range(1, len(report)) if report[i] < report[i - 1])
+        dec_inconsistencies = sum(1 for i in range(1, len(report)) if report[i] > report[i - 1])
+        # check if the step is within bounds
+        step = [1 <= abs(report[i] - report[i - 1]) <= 3 for i in range(1, len(report))]
+        
+        #descide to accept one faulty level
+        if step.count(False) < 1:           
+            if inc_inconsistencies <= 1 or dec_inconsistencies <= 1:
+                count += 1
+        elif step.count(False) <= 1:
+            if inc_inconsistencies < 1 or dec_inconsistencies < 1:
+                count += 1
+    
+    return count
+
+print(lenient_report(file))
